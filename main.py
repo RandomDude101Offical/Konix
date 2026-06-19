194eca9ce214c9bf52d2d06fed54c7ce1df142b5
RandomDude101 <127625280+RandomDude101Offical@users.noreply.github.com>
2026-06-19 13:19:58 +0300

    Add route to tell something nice

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/lifecaffeine/tell_smth_nice')
+def tell_something_nice():
+    return "your smart ;)"
+
 if __name__ == '__main__':
     app.run()
