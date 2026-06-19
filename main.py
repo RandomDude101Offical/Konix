dd7614d7c61ed8874c03841f40d2c68b731ceecc
RandomDude101 <127625280+RandomDude101Offical@users.noreply.github.com>
2026-06-19 14:03:06 +0300

    Enhance tell_something_nice with random messages
    Add random nice messages to the tell_something_nice route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
@@ -8,7 +9,23 @@ def hello_world():
 
 @app.route('/lifecaffeine/tell_smth_nice')
 def tell_something_nice():
-    return "your smart ;)"
+    starts = [
+        "Hi there, ",
+        "Greetings, "
+    ]
+    middles = [
+        "you aren't like anyone else, you are unique, meaning you're special. Out of every single person on earth, who looks exactly the same as next one, you look like yourself, and that's a good thing!",
+        "how's life going? Probably not, but also probably yes, doesn't matter anyway. What I want to deliever to you is that you can always express any feeling you want, because why wouldn't you?"
+    ]
+    end = [
+        " Unfortunately I will have to interrupt nicetelling to tell you that LifeCaffeine is still in beta, but still, come back next day :-)"
+    ]
+
+    partone = random.choice(starts)
+    parttwo = random.choice(middles)
+    partthree = random.choice(end)
+
+    return partone + parttwo + partthree
 
 if __name__ == '__main__':
     app.run()
